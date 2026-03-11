3f0c843bf5a90c758b44348ff9230fb58f845a94
lily-alt-del <santos.miguel1@aluno.ifsp.edu.br>
2026-03-11 15:05:42 -0300

    module tasks

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('teste')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('oi')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
